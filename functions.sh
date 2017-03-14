596129c1410e70379e96ab51ba29014f163d9481
Jeff Kiiza <kiizaa@gmail.com>
2017-03-13 23:51:05 -0300

    mod

--- functions.sh
+++ functions.sh
@@ -10,14 +10,14 @@ genSpider() {
         #url='`echo "$3" | perl -pe "s/\//\\\//g"`'
         #echo $url
 
-        cat template.py.tpl         | \
+        cat templates/spider.py.tpl  | \
         perl -pe "s/{{name}}/$1/g"   | \
         perl -pe "s/{{domain}}/$2/g" | \
         #perl -pe "s/{{url}}/$url/"  | \
         python -c "import sys; print sys.stdin.read().replace('{{url}}', '$url')" | \
         perl -pe "s/{{item}}/$4/g" > numbeo/spiders/$1.py
 
-        cat item.py.tpl         | \
+        cat templates/item.py.tpl    | \
         perl -pe "s/{{item}}/$4/g" >> numbeo/items.py
     fi
 }
